92cdbc4c4818e66dea3d4b4d3f7ea2e564312fd3
Quang Huy <huynq.charles@gmail.com>
2022-09-07 21:34:32 +0700

    Complete Index view of learn kanji

--- KuanfiKanjiWeb/Controllers/LearnKanjiController.cs
+++ KuanfiKanjiWeb/Controllers/LearnKanjiController.cs
@@ -1,6 +1,8 @@
 ﻿using KuanfiKanjiWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using System.Runtime.CompilerServices;
 
 namespace KuanfiKanjiWeb.Controllers
 {
@@ -25,15 +27,43 @@ namespace KuanfiKanjiWeb.Controllers
         }
 
         // Get info of lession set: number of words learned, not learned...
-        public (int, int, int) getLessonInfo()
+        public (int, int, int) getLessonInfo(int set)
         {
-            return (0, 0, 0);
+            int totalWords = _db.RelatedWords.Where(x => x.Set.Equals(set))
+                .Select(x => x.KanjiWord)
+                .Distinct().Count();
+            int learned = _db.RelatedWords.Where(x => x.Set.Equals(set))
+                .Where(x => x.LearnedCount > 0)
+                .Select(x => x.KanjiWord)
+                .Distinct().Count();
+            int notLearned = _db.RelatedWords.Where(x => x.Set.Equals(set))
+                .Where(x => x.LearnedCount == 0)
+                .Select(x => x.KanjiWord)
+                .Distinct().Count();
+            return (totalWords, learned, notLearned);
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? value)
         {
             var kanjiSet = listKanjiSet();
+
+            if (value != null)
+            {
+                kanjiSet.Where(i => i.Value == value.ToString()).First().Selected = true;
+            }
+            else
+            {
+                value = 1;
+            }
+
             ViewBag.kanjiSet = kanjiSet;
+
+            (int totalWords, int learned, int notLearned) = getLessonInfo((int)value);
+
+            ViewBag.totalWords = totalWords;
+            ViewBag.learned = learned;
+            ViewBag.notLearned = notLearned;
+
             return View();
         }
     }
